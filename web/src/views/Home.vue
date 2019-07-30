cd87e01eb3a10b597af83c096bd35434484125bd
BiaoGeSir <392074143@qq.com>
2019-07-30 21:02:55 +0800

    2019-07-30

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,13 +1,44 @@
 <template>
   <div>
-    首页
+    <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide>
+      <img class="w-100" src="../assets/images/banner1.jpeg">
+    </swiper-slide>
+    <swiper-slide>
+      <img class="w-100" src="../assets/images/banner2.jpeg">
+    </swiper-slide>
+    <swiper-slide>
+      <img class="w-100" src="../assets/images/banner3.jpeg">
+    </swiper-slide>
+    <div class="swiper-pagination pagination-home text-right px-3 pb-1"  slot="pagination"></div>
+  </swiper>
   </div>
 </template>
 <script>
 export default {
-  
+  data() {
+    return {
+      swiperOption: {
+        pagination:{
+          el: ".pagination-home"
+        }
+      }
+    }
+  },
 }
 </script>
-<style>
-  
+<style lang="scss">
+@import '../assets/scss/variables';
+
+.pagination-home{
+  .swiper-pagination-bullet{
+    opacity: 1;
+    border-radius: 0.1538rem;
+    background: map-get($colors,'white');
+    &.swiper-pagination-bullet-active{
+      background: map-get($colors,'info');
+    }
+  }
+}
 </style>
