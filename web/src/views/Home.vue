4088bb041b7b0fd7606cd3fb75d983ac34093546
BiaoGeSir <392074143@qq.com>
2019-08-01 21:07:20 +0800

    2019-08-01 PM

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,33 +1,64 @@
 <template>
   <div>
     <swiper :options="swiperOption">
-    <!-- slides -->
-    <swiper-slide>
-      <img class="w-100" src="../assets/images/banner1.jpeg">
-    </swiper-slide>
-    <swiper-slide>
-      <img class="w-100" src="../assets/images/banner2.jpeg">
-    </swiper-slide>
-    <swiper-slide>
-      <img class="w-100" src="../assets/images/banner3.jpeg">
-    </swiper-slide>
-    <div class="swiper-pagination pagination-home text-right px-3 pb-1"  slot="pagination"></div>
-  </swiper>
-  <!-- end of swiper -->
-  <div class="nav-icons bg-white mt-3  text-center pt-3 text-dark-1">
-    <div class="d-flex flex-wrap">
-      <div class="nav-item mb-3" v-for="item in 10" :key="item">
-        <i class="sprite sprite-news"></i>
-        <div class="py-2">爆料站</div>
+      <!-- slides -->
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/banner1.jpeg">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/banner2.jpeg">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/banner3.jpeg">
+      </swiper-slide>
+      <div class="swiper-pagination pagination-home text-right px-3 pb-1"  slot="pagination"></div>
+    </swiper>
+    <!-- end of swiper -->
+    <div class="nav-icons bg-white mt-3  text-center pt-3 text-dark-1">
+      <div class="d-flex flex-wrap">
+        <div class="nav-item mb-3" v-for="item in 10" :key="item">
+          <i class="sprite sprite-news"></i>
+          <div class="py-2">爆料站</div>
+        </div>
+      </div>
+      <div class="bg-light py-2 fs-sm ai-center jc-center">
+        <i class="sprite sprite-arrow mr-1"></i>
+        <span>收起</span>
       </div>
     </div>
-    <div class="bg-light py-2 fs-sm ai-center jc-center">
-      <i class="sprite sprite-arrow mr-1"></i>
-      <span>收起</span>
-    </div>
-  </div>
-  <!-- end of nav icons -->
-    <i class="iconfont icon-caidan1"></i>
+    <!-- end of nav icons -->
+    <m-card icon="caidan1" title="新闻资讯">
+      <div class="nav jc-between">
+        <div class="nav-item active">
+          <div class="nav-link">热门</div>
+        </div>
+        <div class="nav-item">
+          <div class="nav-link">新闻</div>
+        </div>
+        <div class="nav-item">
+          <div class="nav-link">新闻</div>
+        </div>
+        <div class="nav-item">
+          <div class="nav-link">新闻</div>
+        </div>
+      </div>
+      <div class="pt-3">
+        <swiper>
+          <swiper-slide  v-for="m in 5" :key="m">
+              <div class="py-2" v-for="n in 5" :key="n">
+              <span>[新闻]</span>
+              <span>|</span>
+              <span>夏日新版本"稷下星之队"即将6月份上线</span>
+              <span>06/02</span>
+              </div>
+          </swiper-slide>
+        </swiper>
+      </div>
+    </m-card>
+    <m-card icon="card-hero" title="英雄列表"></m-card>
+    <m-card icon="caidan1" title="图文攻略"></m-card>
+    <m-card icon="card-hero" title="英雄列表"></m-card>
+    <m-card icon="card-hero" title="英雄列表"></m-card>
   </div>
 </template>
 <script>
